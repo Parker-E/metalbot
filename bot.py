3c84c37d79971e85184f96fd564ee5e26c67a10c
= <parkerescalette@gmail.com>
2019-06-29 06:26:43 +0100

    Cleaned initial fork to suit this projects needs

---
+++ bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import music
+
+TOKEN = 'NTkzOTM4NTU5OTk2Nzg4NzY2.XRVKxg.G3zzPadn-m1tKgEGyfeLfDhSrSk'
+
+description = '''metalbot QwQ'''
+bot = commands.Bot(command_prefix='~', description=description)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+COGS = [music.Music]
+
+
+def add_cogs(bot):
+    for cog in COGS:
+        bot.add_cog(cog(bot))
+
+add_cogs(bot)
+bot.run(TOKEN)
+
